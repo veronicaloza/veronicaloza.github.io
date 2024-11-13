497a41053e4e2190c8e093342270ad01f0cb49d6
veronicaloza <lozav919@newschool.edu>
2024-11-12 22:42:57 -0500

    updates

--- Entry-8/Entry-8/javascript.js
+++ Entry-8/Entry-8/javascript.js
@@ -1,4 +1,4 @@
-let images = ['ocean-waves.jpg', 'fish.jpeg', 'ripple.jpeg'];
+let images = ['ocean-waves.jpg', 'fish.jpeg', 'ripple.jpeg', 'Michael Kukla.jpeg', 'bread.jpeg'];
 
 console.log(images);
 
@@ -6,11 +6,18 @@ let rectangle = document.getElementById('rectangle');
 
 let wave = document.getElementById('wavy-line');
 
+let sea = document.getElementById('sea');
+
+let secondrectangle = document.getElementById('rectangletwo')
+
 rectangle.style.backgroundImage = `url('${images[Math.floor(Math.random() * images.length)]}')`;
 
 wave.style.backgroundImage = `url('${images[Math.floor(Math.random() * images.length)]}')`;
+rectangletwo.style.backgroundImage = `url('${images[Math.floor(Math.random() * images.length)]}')`;
+
+let palette = ['#163655', '#401E01', '#D9C3B0', '#F2F2F2'];
+
+sea.style.color = palette[Math.floor(Math.random() * palette.length)];
 
 
 
-
-let pallete = ['#163655', '#401E01', '#D9C3B0', '#F2F2F2']
