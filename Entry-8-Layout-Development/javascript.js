d75562dd4d95efaa613396a5d4aadadd3534de4a
veronicaloza <lozav919@newschool.edu>
2024-11-19 16:15:31 -0500

    changes

---
+++ Entry-8-Layout-Development/javascript.js
@@ -0,0 +1,28 @@
+//setInterval(animateRandomness, 1500);
+
+function animateRandomness() {
+
+    let images = ['ocean-waves.jpg', 'fish.jpeg', 'ripple.jpeg', 'Michael Kukla.jpeg', 'bread.jpeg'];
+
+    console.log(images);
+
+    let rectangle = document.getElementById('rectangle');
+
+    let wave = document.getElementById('wavy-line');
+
+    let sea = document.getElementById('sea');
+
+    let secondrectangle = document.getElementById('rectangletwo')
+
+    rectangle.style.backgroundImage = `url('${images[Math.floor(Math.random() * images.length)]}')`;
+
+    wave.style.backgroundImage = `url('${images[Math.floor(Math.random() * images.length)]}')`;
+    rectangletwo.style.backgroundImage = `url('${images[Math.floor(Math.random() * images.length)]}')`;
+
+    let palette = ['#163655', '#401E01', '#D9C3B0', '#F2F2F2'];
+
+    sea.style.color = palette[Math.floor(Math.random() * palette.length)];
+
+    // alert("Refresh Page for More Visual Texture"); // 
+
+}
